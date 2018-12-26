ad4e2f4b9da883cf16386f0a1b61a76df58fb4ce
Joseph Lionarons <lionsamurai@gmail.com>
2018-12-26 13:44:57 -0500

    working on navbar structure

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -4,16 +4,17 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const Home = () => <h2>I'm home!</h2>;
 const About = () => <h2>About me</h2>;
+const Portfolio = () => <h2>This is where projects and stuff go!</h2>;
 const Stuff = () => <h2>Here's some stuff</h2>;
 
 const Main = () => (
   <Router>
-    <div>
+    <div className="container">
       <Navbar />
-      <div>Hello, world! I am inside the Main component.</div>
       <hr />
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
+      <Route exact path="/portfolio" component={Portfolio} />
       <Route exact path="/stuff" component={Stuff} />
     </div>
   </Router>
--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <ul>
+  <ul id="nav-ul">
     <li>
       <Link to="/">Home</Link>
     </li>
     <li>
       <Link to="/about">About</Link>
+    </li>
+    <li>
+      <Link to="/portfolio">Portfolio</Link>
     </li>
     <li>
       <Link to="/stuff">Other Link</Link>

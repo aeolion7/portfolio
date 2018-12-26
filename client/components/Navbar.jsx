672b0815665314568fff7ecafd579cfe5f994997
Joseph Lionarons <lionsamurai@gmail.com>
2018-12-26 14:42:06 -0500

    separated navbar links

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -3,21 +3,25 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <ul id="nav-ul">
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-    <li>
-      <Link to="/about">About</Link>
-    </li>
-    <li>
-      <Link to="/portfolio">Portfolio</Link>
-    </li>
-    <li>
-      <Link to="/stuff">Other Link</Link>
-    </li>
-    <li>
-      <Link to="/stuff">Another Link</Link>
-    </li>
+    <div id="nav-title">
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+    </div>
+    <div id="nav-links">
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/portfolio">Portfolio</Link>
+      </li>
+      <li>
+        <Link to="/stuff">Other Link</Link>
+      </li>
+      <li>
+        <Link to="/stuff">Another Link</Link>
+      </li>
+    </div>
   </ul>
 );
 

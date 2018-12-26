1e3dd5aef6787cf03df4fe4ddb8bdb67e926d1b9
Joseph Lionarons <lionsamurai@gmail.com>
2018-12-26 13:11:37 -0500

    setting up react router

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Navbar from './Navbar.jsx';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+const Home = () => <h2>I'm home!</h2>;
+const About = () => <h2>About me</h2>;
+const Stuff = () => <h2>Here's some stuff</h2>;
 
 const Main = () => (
-  <>
-    <Navbar />
-    <div>Hello, world! I am inside the Main component.</div>
-  </>
+  <Router>
+    <div>
+      <Navbar />
+      <div>Hello, world! I am inside the Main component.</div>
+      <hr />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/stuff" component={Stuff} />
+    </div>
+  </Router>
 );
 
 export default Main;
--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <div>Hello, world! I am inside the Navbar component.</div>
+  <ul>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+    <li>
+      <Link to="/about">About</Link>
+    </li>
+    <li>
+      <Link to="/stuff">Other Link</Link>
+    </li>
+  </ul>
 );
 
 export default Navbar;

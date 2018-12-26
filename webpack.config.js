3fa4e8bd02c26eae33257b5026eb160c4de5179c
Joseph Lionarons <lionsamurai@gmail.com>
2018-12-26 11:42:29 -0500

    creatd webpack config

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+import path from 'path';
+
+module.exports = {
+  entry: './client/index.js',
+  mode: 'development',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  devtool: 'source-maps',
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};

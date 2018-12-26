3e9cb160ba989877e7234825a747f6e04721d212
Joseph Lionarons <lionsamurai@gmail.com>
2018-12-26 12:31:32 -0500

    created Main and Navbar components

---
+++ client/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from './Navbar.jsx';
+
+const Main = () => (
+  <>
+    <Navbar />
+    <div>Hello, world! I am inside the Main component.</div>
+  </>
+);
+
+export default Main;
---
+++ client/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Navbar = () => (
+  <div>Hello, world! I am inside the Navbar component.</div>
+);
+
+export default Navbar;

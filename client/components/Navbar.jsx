29c30f409edfd496fc3ccffe10aeb019ba17041d
Joseph Lionarons <lionsamurai@gmail.com>
2018-12-27 19:13:34 -0500

    added header tag

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -2,30 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <ul id="nav-ul">
-    <div id="nav-title">
-      <li>
-        <Link to="/">
-          <p id="title-name">Joseph Lionarons</p>
-          <p id="title-field">Full Stack Software Engineer</p>
-        </Link>
-      </li>
-    </div>
-    <div id="nav-links">
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/portfolio">Portfolio</Link>
-      </li>
-      <li>
-        <Link to="/contact">Contact</Link>
-      </li>
-      <li>
-        <Link to="/stuff">Another Link</Link>
-      </li>
-    </div>
-  </ul>
+  <header>
+    <ul id="nav-ul">
+      <div id="nav-title">
+        <li>
+          <Link to="/">
+            <h1 id="title-name">Joseph Lionarons</h1>
+            <p id="title-field">Full Stack Software Engineer</p>
+          </Link>
+        </li>
+      </div>
+      <div id="nav-links">
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/stuff">Another Link</Link>
+        </li>
+      </div>
+    </ul>
+  </header>
 );
 
 export default Navbar;
